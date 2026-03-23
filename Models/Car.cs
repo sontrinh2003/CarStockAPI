4231309cacb736b8d2d0e7a22ac591021a0523f4
sontrinh2003 <hoangson200403@gmail.com>
2026-03-24 02:14:51 +1100

    Add model classes

---
+++ Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace CarStockAPI.Models
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public int DealerId { get; set; }
+        public string Make { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public int Year { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ Models/Dealer.cs
@@ -0,0 +1,10 @@
+﻿namespace CarStockAPI.Models
+{
+    public class Dealer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+    }
+}
